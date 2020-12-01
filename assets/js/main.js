9e9f6d90850d0fb1c59665e7acd20a6d1a21e068
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-01 16:34:01 +0100

    Sostituito la serie di if con un ciclo for
    Sostituito la serie di if con un ciclo for

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,44 +17,51 @@ $(function (){
     //Creata una funzione che fa un conteggio alla rovescia
     setTimeout(conteggio, 2000);
     function conteggio() {
-        var listanumeriUtente = [];
+        var listaNumeriUtente = [];
         //terminato il conteggio chiede all' utente di inserire per 5 volte un numero
         for (var index = 0; index <= 4; index++) {
             var numeroUtente = Number(prompt('Inserisci un numero che ricordi'));
             //inseriamo il numero scritto all'interno di un array
-            listanumeriUtente.push(numeroUtente);
+            listaNumeriUtente.push(numeroUtente);
         };
-        console.log(listanumeriUtente);
+        console.log(listaNumeriUtente);
         
         //lista in cui verranno inseriti i numeri che si trovano in entrambe le liste 
         var listaFinale = [];
 
-        // serie di if
+        // // serie di if
 
-        //if che serve a verificare se il primo elemento di "listaNumeriUtente" si trova all'interno di "listaNumeriCasuali"
-        if(listanumeriUtente[0] == listaNumeriCasuali[0] || listanumeriUtente[0] == listaNumeriCasuali[1] || listanumeriUtente[0] == listaNumeriCasuali[2] || listanumeriUtente[0] == listaNumeriCasuali[3] || listanumeriUtente[0] == listaNumeriCasuali[4]){
-            listaFinale.push(listanumeriUtente[0]);
+        // //if che serve a verificare se il primo elemento di "listaNumeriUtente" si trova all'interno di "listaNumeriCasuali"
+        // if(listaNumeriUtente[0] == listaNumeriCasuali[0] || listaNumeriUtente[0] == listaNumeriCasuali[1] || listaNumeriUtente[0] == listaNumeriCasuali[2] || listaNumeriUtente[0] == listaNumeriCasuali[3] || listaNumeriUtente[0] == listaNumeriCasuali[4]){
+        //     listaFinale.push(listaNumeriUtente[0]);
+        // };
+        // //if che serve a verificare se il secondo elemento di "listaNumeriUtente" si trova all'interno di "listaNumeriCasuali"
+        // if(listaNumeriUtente[1] == listaNumeriCasuali[0] || listaNumeriUtente[1] == listaNumeriCasuali[1] || listaNumeriUtente[1] == listaNumeriCasuali[2] || listaNumeriUtente[1] == listaNumeriCasuali[3] || listaNumeriUtente[1] == listaNumeriCasuali[4]){
+        //     listaFinale.push(listaNumeriUtente[1]);
+        // };
+        // //if che serve a verificare se il terzo elemento di "listaNumeriUtente" si trova all'interno di "listaNumeriCasuali"
+        // if(listaNumeriUtente[2] == listaNumeriCasuali[0] || listaNumeriUtente[2] == listaNumeriCasuali[1] || listaNumeriUtente[2] == listaNumeriCasuali[2] || listaNumeriUtente[2] == listaNumeriCasuali[3] || listaNumeriUtente[2] == listaNumeriCasuali[4]){
+        //     listaFinale.push(listaNumeriUtente[2]);
+        // };
+        // //if che serve a verificare se il quarto elemento di "listaNumeriUtente" si trova all'interno di "listaNumeriCasuali"
+        // if(listaNumeriUtente[3] == listaNumeriCasuali[0] || listaNumeriUtente[3] == listaNumeriCasuali[1] || listaNumeriUtente[3] == listaNumeriCasuali[2] || listaNumeriUtente[3] == listaNumeriCasuali[3] || listaNumeriUtente[3] == listaNumeriCasuali[4]){
+        //     listaFinale.push(listaNumeriUtente[3]);
+        // };
+        // //if che serve a verificare se il quinto elemento di "listaNumeriUtente" si trova all'interno di "listaNumeriCasuali"
+        // if(listaNumeriUtente[4] == listaNumeriCasuali[0] || listaNumeriUtente[4] == listaNumeriCasuali[1] || listaNumeriUtente[4] == listaNumeriCasuali[2] || listaNumeriUtente[4] == listaNumeriCasuali[3] || listaNumeriUtente[4] == listaNumeriCasuali[4]){
+        //     listaFinale.push(listaNumeriUtente[4]);
+        // };
+        // console.log(listaFinale);
+
+        //sostituita la serie di if con un ciclo for
+        for (var y = 0; y < listaNumeriCasuali.length; y++) {
+            if (listaNumeriCasuali.includes(listaNumeriUtente[y])) {
+                listaFinale.push(listaNumeriUtente[y]);
+            };
+            
         };
-        //if che serve a verificare se il secondo elemento di "listaNumeriUtente" si trova all'interno di "listaNumeriCasuali"
-        if(listanumeriUtente[1] == listaNumeriCasuali[0] || listanumeriUtente[1] == listaNumeriCasuali[1] || listanumeriUtente[1] == listaNumeriCasuali[2] || listanumeriUtente[1] == listaNumeriCasuali[3] || listanumeriUtente[1] == listaNumeriCasuali[4]){
-            listaFinale.push(listanumeriUtente[1]);
-        };
-        //if che serve a verificare se il terzo elemento di "listaNumeriUtente" si trova all'interno di "listaNumeriCasuali"
-        if(listanumeriUtente[2] == listaNumeriCasuali[0] || listanumeriUtente[2] == listaNumeriCasuali[1] || listanumeriUtente[2] == listaNumeriCasuali[2] || listanumeriUtente[2] == listaNumeriCasuali[3] || listanumeriUtente[2] == listaNumeriCasuali[4]){
-            listaFinale.push(listanumeriUtente[2]);
-        };
-        //if che serve a verificare se il quarto elemento di "listaNumeriUtente" si trova all'interno di "listaNumeriCasuali"
-        if(listanumeriUtente[3] == listaNumeriCasuali[0] || listanumeriUtente[3] == listaNumeriCasuali[1] || listanumeriUtente[3] == listaNumeriCasuali[2] || listanumeriUtente[3] == listaNumeriCasuali[3] || listanumeriUtente[3] == listaNumeriCasuali[4]){
-            listaFinale.push(listanumeriUtente[3]);
-        };
-        //if che serve a verificare se il quinto elemento di "listaNumeriUtente" si trova all'interno di "listaNumeriCasuali"
-        if(listanumeriUtente[4] == listaNumeriCasuali[0] || listanumeriUtente[4] == listaNumeriCasuali[1] || listanumeriUtente[4] == listaNumeriCasuali[2] || listanumeriUtente[4] == listaNumeriCasuali[3] || listanumeriUtente[4] == listaNumeriCasuali[4]){
-            listaFinale.push(listanumeriUtente[4]);
-        };
-        console.log(listaFinale);
 
         alert("Hai ricordato " + listaFinale.length + " numeri e sono: " + listaFinale);
-        
     };
 
 });
