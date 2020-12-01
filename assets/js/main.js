147464f7a57a29dce4a8dd1b385b531a4e9cd953
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-01 14:05:30 +0100

    Creata una funzione che genera 5 numeri casuali diversi
    Creata una funzione che genera 5 numeri casuali diversi

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,16 @@
 // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
 $(function (){
+    //funzione che genera 5numeri casuali compresi tra 1 e 100
+    var numeroCasuale;
+    var listaNumeriCasuali = [];
+    for(i= 0; i <= 4; i++){
+        while ( listaNumeriCasuali.includes( numeroCasuale = Math.floor(Math.random()*100) + 1 ) );
+        listaNumeriCasuali[i] = numeroCasuale;
+    };
+
+    //alert che espone 5 numeri casuali diversi
+    alert(listaNumeriCasuali);
 
 
-
-
-    
 });
