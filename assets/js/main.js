f6efbc0ab12f6f5e13049b5ddeb1a5f3f8a0522e
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-12-01 14:28:29 +0100

    Creata una funzione di conteggio
    Creata una funzione di conteggio, al termine del conteggio chiede all'utente per 5 volte di inserire un numero

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ $(function (){
     //funzione che genera 5numeri casuali compresi tra 1 e 100
     var numeroCasuale;
     var listaNumeriCasuali = [];
-    for(i= 0; i <= 4; i++){
+    for(i = 0; i <= 4; i++){
         while ( listaNumeriCasuali.includes( numeroCasuale = Math.floor(Math.random()*100) + 1 ) );
         listaNumeriCasuali[i] = numeroCasuale;
     };
@@ -14,5 +14,16 @@ $(function (){
     //alert che espone 5 numeri casuali diversi
     alert(listaNumeriCasuali);
 
+    //Creata una funzione che fa un conteggio alla rovescia
+    setTimeout(conteggio, 2000);
+    function conteggio() {
+        //terminato il conteggio chiede all' utente di inserire per 5 volte un numero
+        for (var index = 0; index <= 4; index++) {
+            var numeriUtente = Number(prompt('Inserisci un numero che ricordi'));
+            
+        }
+    }
 
+
+    
 });
